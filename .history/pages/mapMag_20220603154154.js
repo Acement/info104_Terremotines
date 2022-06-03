c4a63b43977eafb30848c5b370317283f9b3b0fb
Maxi Arriagada <maxiarriagada.p@outlook.com>
2022-06-03 15:42:04 -0400

    cambios mapa y marcadores

---
+++ .history/pages/mapMag_20220603154154.js
@@ -0,0 +1,77 @@
+
+
+import MainLayout from "../components/mainLayout";
+import React, { useState } from "react";
+import{
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+
+import Mag from "../public/data/magnitud.json";
+import MStyles from "../public/data/MStyles";
+const mapContainerStyle ={
+  width: "100vw",
+  height: "100vh",
+
+};
+const libraries = ["places"];
+const centro ={
+  lat: 0.000000,
+  lng: 0.000000,
+};
+const options={
+  styles: MStyles,
+  disableDefaultUI: true,
+};
+
+export default function main() {
+    const [SelectedMarker,setSelectedMarker]= useState(null);
+    const {isLoaded, loadError } = useLoadScript({
+      googleMapsApiKey: process.env.NEXT_PUBLIC_MY_API_KEY,
+      libraries,
+    });
+  if (loadError) return "error al cargar mapa";
+  if (!isLoaded) return  "Cargando el mapa";
+
+
+  return (
+    <MainLayout pageId="main">
+      <h1 
+      >Terremotos </h1>
+      <GoogleMap 
+      mapContainerStyle={mapContainerStyle} 
+      zoom= {3}
+      center={centro}
+      options={options}
+      >
+      {Mag.Terremotos.map((EqMag)=>(
+              <Marker key={EqMag.Datos.EQ_ID} 
+                      position={{lat:EqMag.geometry.coordinates[0],
+                                lng:EqMag.geometry.coordinates[1]}}
+                      onClick={()=>
+                          {setSelectedMarker(EqMag);
+                      }}/>
+          
+          ))}
+          {SelectedMarker &&<InfoWindow 
+            position={{
+              lat:SelectedMarker.geometry.coordinates[0],
+              lng:SelectedMarker.geometry.coordinates[1]
+              }}
+              onClick= {()=>{
+                setSelectedMarker(null);
+              }}
+              >
+            <div>
+              <b>{SelectedMarker.Datos.NAME}</b>
+              <p>{SelectedMarker.Datos.INFO}</p>
+            </div>
+          </InfoWindow>
+           }
+      </GoogleMap>
+
+    </MainLayout>
+  );
+}
---
+++ .history/pages/mapMor_20220603154154.js
@@ -0,0 +1,80 @@
+
+
+import MainLayout from "../components/mainLayout";
+import React, { useState } from "react";
+import{
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+
+import Mor from "../public/data/victimas.json";
+
+import MStyles from "../public/data/MStyles";
+const mapContainerStyle ={
+  width: "100vw",
+  height: "100vh",
+
+};
+const libraries = ["places"];
+const centro ={
+  lat: 0.000000,
+  lng: 0.000000,
+};
+const options={
+  styles: MStyles,
+  disableDefaultUI: true,
+};
+
+export default function main() {
+    const [SelectedMarker,setSelectedMarker]= useState(null);
+    const {isLoaded, loadError } = useLoadScript({
+      googleMapsApiKey: process.env.NEXT_PUBLIC_MY_API_KEY,
+      libraries,
+    });
+  if (loadError) return "error al cargar mapa";
+  if (!isLoaded) return  "Cargando el mapa";
+
+
+  return (
+    <MainLayout pageId="main">
+      <h1 
+      >Terremotos </h1>
+      <GoogleMap 
+      mapContainerStyle={mapContainerStyle} 
+      zoom= {3}
+      center={centro}
+      options={options}
+      >
+          {Mor.Terremotos.map((EqMor)=>(
+              <Marker key={EqMor.Datos.EQ_ID} 
+                      position={{lat:EqMor.geometry.coordinates[0],
+                                lng:EqMor.geometry.coordinates[1]}} 
+                      onClick={()=>
+                          {setSelectedMarker(EqMor);
+                          
+                      }}
+                        />
+
+          ))}
+          {SelectedMarker &&<InfoWindow 
+            position={{
+              lat:SelectedMarker.geometry.coordinates[0],
+              lng:SelectedMarker.geometry.coordinates[1]
+              }}
+              onClick= {()=>{
+                setSelectedMarker(null);
+              }}
+              >
+            <div>
+              <b>{SelectedMarker.Datos.NAME}</b>
+              <p>{SelectedMarker.Datos.INFO}</p>
+            </div>
+          </InfoWindow>
+           }
+      </GoogleMap>
+
+    </MainLayout>
+  );
+}
