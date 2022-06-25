a598e13294d0d0cd8db6cb3c44f8ee12b3e86d5f
unknown <pj.cisterna@gmail.com>
2022-06-25 19:57:37 -0400

    Agregado segundo grafico

--- pages/graficos.js
+++ pages/graficos.js
@@ -83,25 +83,6 @@ export default function tabla1() {
           <Line type="monotone" dataKey="magnitud" stroke="#8884d8" activeDot={{ r: 8 }} />
           <Line type="monotone" dataKey="victimas" stroke="#82ca9d" />
         </LineChart>
-        <LineChart
-          width={300}
-          height={200}
-          data={Exp}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="magnitud" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="costo" stroke="#82ca9d" />
-        </LineChart>
       </ResponsiveContainer>
 
 
