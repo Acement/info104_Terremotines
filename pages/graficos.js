03cca5630a10b8377346641784849eaf2fff5ee5
Cfelix61 <cristian.oyarce@alumnos.uach.cl>
2022-06-25 20:00:16 -0400

    Agregando mas grafiicos

--- pages/graficos.js
+++ pages/graficos.js
@@ -85,6 +85,28 @@ export default function tabla() {
           <Line type="monotone" dataKey="victimas" stroke="#82ca9d" />
         </LineChart>
       </ResponsiveContainer>
+      
+      <ResponsiveContainer width="100%" aspect={3}>
+        <LineChart
+          width={300}
+          height={200}
+          data={data1}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="magnitud" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="victimas" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
 
 
     </MainLayout>
